ba7daf36bc166c54ab99cb429d8ef813969e3344
rifengmen <438944081@qq.com>
2019-07-29 17:13:07 +0800

    修改配置信息

--- config/index.js
+++ config/index.js
@@ -10,7 +10,17 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'http://localhost:8080/',//设置你调用的接口域名和端口号 别忘了加http
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/'
+          //这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调
+          //用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
@@ -50,13 +60,13 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
      */
 
-    productionSourceMap: true,
+    productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
     devtool: '#source-map',
 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+import axios from 'axios'
+axios.defaults.baseURL = url;//这里写上自己的基础url，例如www.csdn.com
+Vue.prototype.$axios = axios;
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+import qs from 'qs';
+Vue.prototype.qs = qs;
 
 /* eslint-disable no-new */
 new Vue({
