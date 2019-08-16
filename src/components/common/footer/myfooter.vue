3f3198fcb5f712d7f4cd971b63b132b24eaaa04b
rifengmen <438944081@qq.com>
2019-08-16 15:01:07 +0800

    进度上传

--- src/components/common/footer/myfooter.vue
+++ src/components/common/footer/myfooter.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="footer bgfff">
     <div class="footer-main">
-      <router-link :to="{name:'index'}" tag="div" class="shops tc">
+      <router-link :to="{name:'index'}" tag="div" class="footer_shops tc">
         <div class="bgimg"></div>
         <!--<img src="static/img/index.png">-->
         <div class="listname font24 color666">商铺</div>
       </router-link>
-      <router-link :to="{name:'listSupplier'}" tag="div" class="supplier tc">
+      <router-link :to="{name:'listSupplier'}" tag="div" class="footer_supplier tc">
         <div class="bgimg"></div>
         <!--<img src="static/img/supplier.png">-->
         <div class="listname font24 color666">供应</div>
       </router-link>
-      <router-link :to="{name:'listPurchase'}" tag="div" class="purchase tc">
+      <router-link :to="{name:'listPurchase'}" tag="div" class="footer_purchase tc">
         <div class="bgimg"></div>
         <!--<img src="static/img/purchase.png">-->
         <div class="listname font24 color666">求购</div>
       </router-link>
-      <router-link :to="{name:'userInfo'}" tag="div" class="userinfo tc">
+      <router-link :to="{name:'userInfo'}" tag="div" class="footer_userinfo tc">
         <div class="bgimg"></div>
         <!--<img src="static/img/userinfo.png">-->
         <div class="listname font24 color666">我的</div>
